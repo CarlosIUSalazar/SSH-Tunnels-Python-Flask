5345a7328418fc10e96aaa42c3d902f03b1e9d00
Carlos Salazar <carlosiusalazar@gmail.com>
2020-10-10 16:42:11 +0900

    Added app2.py

---
+++ app2.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app2 = Flask(__name__)
+
+@app2.route('/')
+def index():
+    return '<h1 style="color:red;">This is the the super secret page</h1>'
+
+
+if __name__ == '__main__':
+    app2.run(host='0.0.0.0', port=5002, debug = True)
