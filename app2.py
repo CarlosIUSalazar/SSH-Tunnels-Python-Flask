30c3e0cd3e925ee450721db7fd1e976840cb1d0a
Carlos Salazar <carlosiusalazar@gmail.com>
2020-10-20 16:44:03 +0900

    Added app2.py

--- app2.py
+++ app2.py
@@ -7,4 +7,4 @@ def index():
     return '<h1 style="color:red;">This is the the super secret page</h1>'
 
 if __name__ == '__main__':
-    app2.run(host='0.0.0.0', port=5002)
+    app2.run(host='0.0.0.0', port=5002, debug = True)
